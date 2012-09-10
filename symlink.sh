871bf93f8227c359d166cc45a7b6d34aca5a0b99
t-nakaym <tnakaym@gmail.com>
2012-09-10 11:10:43 +0900

    Add a shellscript to create symlinks at my home directory.

---
+++ symlink.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+cd $(dirname $0)
+for dotfile in .?*; do
+    case $dotfile in
+        ..)
+            continue;;
+        .git)
+            continue;;
+        *)
+            ln -Fis "$PWD/$dotfile" $HOME
+            ;;
+    esac
+done
+

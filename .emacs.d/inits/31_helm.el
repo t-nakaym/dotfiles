fa8146c6d6cf208980df84b85d25dd83db53fd7e
t-nakaym <tnakaym@gmail.com>
2014-04-28 20:02:44 +0900

    Add helm.el settings

--- .emacs.d/inits/31_helm.el
+++ .emacs.d/inits/31_helm.el
@@ -2,5 +2,17 @@
 
 (require 'helm-config)
 
-(global-set-key (kbd "M-;") 'helm-mini)
+(defun my-helm ()
+  (interactive)
+  (require 'helm-files)
+  (let ((helm-ff-transformer-show-only-basename nil))
+    (helm-other-buffer
+     '(helm-source-buffers-list
+       helm-source-recentf
+       helm-source-buffer-not-found
+       helm-source-files-in-current-dir
+       helm-source-file-name-history)
+     "*my helm*")))
+(global-set-key (kbd "M-;") 'my-helm)
+(global-set-key (kbd "M-x") 'helm-M-x)
 (global-set-key (kbd "M-y") 'helm-show-kill-ring)

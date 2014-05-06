47868d75148e834dfbf3122898ff3b419d1bfad2
t-nakaym <tnakaym@gmail.com>
2014-05-06 17:36:26 +0900

    Fix settings for migemo.el

--- .emacs.d/inits/31_migemo.el
+++ .emacs.d/inits/31_migemo.el
@@ -1,4 +1,14 @@
 ;;; 31_migemo.el --- settings for migemo
 
-(require 'migemo)
-(setq migemo-command "/usr/bin/ruby")
+(when (and (executable-find "cmigemo")
+           (require 'migemo nil t))
+  (setq migemo-command "cmigemo")
+  (setq migemo-dictionary "/usr/share/cmigemo/utf-8/migemo-dict")
+  (setq migemo-options '("-q" "--emacs"))
+
+  (setq migemo-user-dictionary nil)
+  (setq migemo-regex-dictionary nil)
+  (setq migemo-coding-system 'utf-8-unix)
+  (load-library "migemo")
+  (migemo-init)
+)

71adf855818a5b04737530140a96124b04abefc5
t-nakaym <tnakaym@gmail.com>
2014-05-09 03:16:49 +0900

    Fix auto-complete settings to adapt installing from Cask

--- .emacs.d/inits/31_auto-complete.el
+++ .emacs.d/inits/31_auto-complete.el
@@ -1,7 +1,5 @@
 ;;; for auto-complete.el
 
-; (package-install 'auto-complete)
-(add-to-list 'load-path "~/.emacs.d/elpa/auto-complete-20130122.1551")
 (require 'auto-complete-config)
-(add-to-list 'ac-dictionary-directories "~/.emacs.d/elpa/auto-complete-20130122.1551/dict")
 (ac-config-default)
+(add-to-list 'ac-modes 'enh-ruby-mode)

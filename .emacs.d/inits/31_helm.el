7fa5d078058137763b3f52307e165564c2b5c470
t-nakaym <tnakaym@gmail.com>
2016-08-16 09:50:56 +0900

    Change helm setting

--- .emacs.d/inits/31_helm.el
+++ .emacs.d/inits/31_helm.el
@@ -3,21 +3,18 @@
 (require 'helm-config)
 (helm-mode 1)
 
-(defun my-helm ()
-  (interactive)
-  (require 'helm-files)
-  (let ((helm-ff-transformer-show-only-basename nil))
-    (helm-other-buffer
-     '(helm-source-buffers-list
-       helm-source-recentf
-       helm-source-buffer-not-found
-       helm-source-files-in-current-dir
-       helm-source-file-name-history)
-     "*my helm*")))
-(global-set-key (kbd "M-;") 'my-helm)
+(custom-set-variables
+ '(helm-mini-default-sources
+   '(helm-source-buffers-list
+     helm-source-recentf
+     helm-source-files-in-current-dir
+     helm-source-file-name-history)))
+(global-set-key (kbd "M-;") 'helm-mini)
 (global-set-key (kbd "M-x") 'helm-M-x)
 (global-set-key (kbd "M-y") 'helm-show-kill-ring)
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
 
 (define-key helm-map (kbd "C-h") 'helm-ff-delete-char-backward)
-(define-key helm-read-file-map (kbd "TAB") 'helm-execute-persistent-action)
+(define-key helm-find-files-map (kbd "C-h") 'delete-backward-char)
 (define-key helm-find-files-map (kbd "TAB") 'helm-execute-persistent-action)
+(define-key helm-read-file-map (kbd "TAB") 'helm-execute-persistent-action)
